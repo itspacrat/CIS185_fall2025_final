df28879675f81e41555a05993da402ce25047868
Jason Rhoads <jasrhoads@gmail.com>
2025-12-06 21:45:56 -0800

    add mute button images, styling, and functionality

--- script.js
+++ script.js
@@ -131,6 +131,38 @@ window.addEventListener('load', function () {
     }
   });
 
+  // Volumn Button controls - toggle the womp womp on and off
+  const volumeButton = document.getElementById('volumeBtn');
+
+  //set the svg for the button keeping the index file cleaner
+  volumeButton.innerHTML = `<img id="volumeIcon" src="/assets/images/volume-high-solid-full.svg" alt="Sound On">`
+
+  const volumeIcon = document.getElementById('volumeIcon');
+
+  // Monitor if the volume is on or off
+  let volumeIsOn = true;
+
+  volumeButton.addEventListener('click', () => {
+    
+    // Volume is on and user clicked it off
+    if (volumeIsOn) {
+      gameOverSound.volume = 0;
+      volumeIcon.src ="/assets/images/volume-xmark-solid-full.svg";
+      volumeIcon.alt = "Sound off";
+      volumeButton.setAttribute("aria-label", "Unmute sound");
+    } else {
+      gameOverSound.volume = 0.1;
+      volumeIcon.src ="/assets/images/volume-high-solid-full.svg";
+      volumeIcon.alt = "Sound on";
+      volumeButton.setAttribute("aria-label", "Mute sound");
+    }
+    
+    volumeIsOn = !volumeIsOn;
+  });
+
+
+
+
 
 
   class InputHandler {
